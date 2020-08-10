c0d35352904208a90f5f45c630dfe5692ab44ba5
Sneha <sneha46749@gmail.com>
2020-08-10 23:26:22 +0530

    Adding navigation menu to easily navigate through the webpages.

--- Navigation-Menu.py
+++ Navigation-Menu.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')  
 def home():  
-    return render_template("home.html")
+    return render_template("home1.html")
 
 @app.route('/about/')    
 def about():
-    return render_template("about.html")
+    return render_template("about1.html")
 
 if __name__=="__main__":
     app.run(debug=True)    
